e9cb4c10fb2c317b791d4498ec747428a19b855d
Jason Benn <jasoncbenn@gmail.com>
2017-04-12 18:45:57 -0300

    Refine Babel dependencies - I'm just trying to compile down to node6.

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,27 @@
 # /bin/bash
 
-# Install Node and NPM
+# Install Node v6
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 68576280
+sudo apt-add-repository "deb https://deb.nodesource.com/node_6.x $(lsb_release -sc) main"
+sudo apt-get update
 sudo apt-get install nodejs
-sudo apt-get install npm
-sudo ln -s /usr/bin/nodejs /usr/bin/node
 
+# Need to run twice, for some reason
+sudo npm install -g npm
+
+# Dumb solution to cloning a private repo: use your own login credentials
 git clone https://github.com/minervaproject/qa-instances.git
 
 # Install dependencies
+sudo apt-get install sqlite3
 npm install
+
+# Add node_module exectables to path
+PATH="/home/ubuntu/qa-instances/node_modules/.bin:$PATH"
 
 # Start DB
 babel-node create-db.js
 
 # Start app
-npm start
+npm run build
+npm run serve
