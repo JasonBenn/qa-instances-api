399c102a995a50478b4fbd1687ec083a702c0909
Jason Benn <jasoncbenn@gmail.com>
2017-04-20 16:57:15 -0300

    Couple of bugfixes.

--- src/aws.js
+++ src/aws.js
@@ -1,6 +1,8 @@
+import { execFile } from 'child_process'
+
 const createDB = () => {
   // https://nodejs.org/api/child_process.html#child_process_child_process_execfile_file_args_options_callback
-  execFile("scripts/create-qa-instance-db.sh", null, {
+  execFile(process.cwd() + "/scripts/create-qa-instance-db.sh", null, {
     env: {
       dbName: "review_features_lo_detail_page"     
     }
