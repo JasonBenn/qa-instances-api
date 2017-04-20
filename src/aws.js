823d1401ab273959db13c8169c2fa0b1ad45fc67
Jason Benn <jasoncbenn@gmail.com>
2017-04-20 16:38:52 -0300

    Experimental DB creation script, called from aws.js.

---
+++ src/aws.js
@@ -0,0 +1,12 @@
+const createDB = () => {
+  // https://nodejs.org/api/child_process.html#child_process_child_process_execfile_file_args_options_callback
+  execFile("scripts/create-qa-instance-db.sh", null, {
+    env: {
+      dbName: "review_features_lo_detail_page"     
+    }
+  }, function(error, stdout, stderr) {
+    console.log(error, stdout, stderr);
+  });
+}
+
+createDB()
