2aae96ddf15b345fa5b424cffe41794c4058f42b
Jason Benn <jasoncbenn@gmail.com>
2017-04-21 11:43:56 -0300

    Guard.

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const rebroadcastCmds = (socket, io) => {
   socket.on('cmd', function(cmd) {
     var matched = /(.*):(.*)/.exec(cmd);
-    if (matched.length >= 3) {
+    if (matched && matched.length >= 3) {
       var channel = matched[1];
       var message = matched[2];
       console.log("received cmd:", cmd, ", channel:", channel, ", message:", message);
