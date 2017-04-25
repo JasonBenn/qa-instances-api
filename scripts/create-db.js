c8ec6302a509b6f952f110b83f5440ecba39060e
Jason Benn <jasoncbenn@gmail.com>
2017-04-24 17:25:10 -0700

    More schema updates.

--- scripts/create-db.js
+++ scripts/create-db.js
@@ -3,18 +3,17 @@ import sqlite3 from 'sqlite3'
 const command = `
   CREATE TABLE pulls (
     id INTEGER PRIMARY KEY,
-    pr_id INTEGER, -- should match github
-    pr_name TEXT, -- should match github
-    hostname TEXT, -- (normalized to be valid for AWS)
-    db_name TEXT, -- normalized to be valid for RDS, might entail truncation
+    prId INTEGER, -- should match github
+    prName TEXT, -- should match github
+    hostName TEXT, -- (normalized to be valid for AWS)
+    dbName TEXT, -- normalized to be valid for RDS, might entail truncation
     instanceState TEXT, -- starting|online|stopping (when no row is found, instance is assumed offline)
     deployState TEXT, -- stopped|setting-up|deploying|created
-    db_username TEXT,
     url TEXT, -- url
     sha TEXT, -- of most recent deploy
-    deploying BOOLEAN,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    UNIQUE(pr_id)
+    UNIQUE(prId),
+    UNIQUE(hostName)
   );
 `
 
