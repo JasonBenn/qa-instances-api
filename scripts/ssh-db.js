f3e295eb8a7eea47ee55886e4c9e65846cbc42d9
Jason Benn <jasoncbenn@gmail.com>
2017-05-06 16:38:02 -0700

    Another fix for ssh-db.

--- scripts/ssh-db.js
+++ scripts/ssh-db.js
@@ -1,9 +1,8 @@
 import { readJSON } from '../src/utils'
-import { exec } from 'child_process'
 import kexec from 'kexec'
 
 
 readJSON('config/picasso.json').then(config => {
   const { dbHost, dbUser, dbPassword, dbName } = config
-  kexec(`mysql -h ${dbHost} -u ${dbUser} -p${dbPassword} ${dbName}`)
+  kexec(`mysql -h ${dbHost} -u ${dbUser} -p${dbPassword}`);
 })
