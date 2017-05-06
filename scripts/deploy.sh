13b2ef51a3bcd923d5181f0e42bb02f47ccea3eb
Jason Benn <jasoncbenn@gmail.com>
2017-05-06 14:34:09 -0700

    Log deploy steps.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,12 @@
 # /bin/bash
 
+echo "forever stopall"
 forever stopall
+echo "yarn install --pure-lockfile"
 yarn install --pure-lockfile  # don't generate a new lockfile (yarn install yields different package orderings on Ubuntu and OSX, and we want a clean git status)
+echo "scp config"
 scp -i ~/.ssh/staging.pem -r config ubuntu@qa-instance-coordinator1.vpcstaging:/home/ubuntu/qa-instances-api/
+echo "npm run build"
 npm run build
+echo "npm run serve"
 npm run serve
