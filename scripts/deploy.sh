fbd1ec905694eff7f8cad5939bd8d63f7c78cdb8
Jason Benn <jasoncbenn@gmail.com>
2017-05-06 14:12:42 -0700

    Add a deploy step to copy up configuration files.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,6 @@
 
 forever stopall
 yarn install --pure-lockfile  # don't generate a new lockfile (yarn install yields different package orderings on Ubuntu and OSX, and we want a clean git status)
+scp -i ~/.ssh/staging.pem -r config ubuntu@qa-instance-coordinator1.vpcstaging:/home/ubuntu/qa-instances-api/
 npm run build
 npm run serve
