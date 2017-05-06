b79fd71d82933ec1aac9326cd7fcf5e8dbc4ad64
Jason Benn <jasoncbenn@gmail.com>
2017-05-06 14:03:37 -0700

    Don't generate a new lockfile (yarn install yields different package orderings on Ubuntu and OSX, and we want a clean git status).

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 # /bin/bash
 
 forever stopall
-yarn install
+yarn install --pure-lockfile  # don't generate a new lockfile (yarn install yields different package orderings on Ubuntu and OSX, and we want a clean git status)
 npm run build
 npm run serve
