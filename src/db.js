f435b89565af4b0d4cf5512572c41b8d22742256
Jason Benn <jasoncbenn@gmail.com>
2017-05-08 14:48:26 -0700

    Log actual DB queries.

--- src/db.js
+++ src/db.js
@@ -17,32 +17,32 @@ export default class DB {
   }
 
   create(data) {
-    if (LOG_QUERIES) console.log(`db: INSERT ${JSON.stringify(data)}`)
     const { keys, questionMarks, values } = this.getQueryTemplateParams(data)
     return new Promise((resolve, reject) => {
+      if (LOG_QUERIES) console.log(`db: INSERT OR IGNORE INTO pulls (${keys}) VALUES (${questionMarks})`, values)
       this.db.run(`INSERT OR IGNORE INTO pulls (${keys}) VALUES (${questionMarks})`, values, promiseCb(resolve, reject))
     })
   }
 
   get(prId) {
-    if (LOG_QUERIES) console.log(`db: SELECT ${prId}`)
     return new Promise((resolve, reject) => {
+      if (LOG_QUERIES) console.log(`db: SELECT * FROM pulls WHERE prId = ?`, prId)
       this.db.get(`SELECT * FROM pulls WHERE prId = ?`, prId, promiseCb(resolve, reject))
     })
   }
 
   update(prId, data) {
-    if (LOG_QUERIES) console.log(`db: UPDATE ${prId} SET ${JSON.stringify(data)}`)
     return new Promise((resolve, reject) => {
       const values = _.map(data, (value, key) => [key, '"' + value + '"'].join(' = ')).join(', ')
+      if (LOG_QUERIES) console.log(`db: UPDATE pulls SET ${values} WHERE prId = ?`, prId)
       this.db.run(`UPDATE pulls SET ${values} WHERE prId = ?`, prId, promiseCb(resolve, reject))
     })
   }
 
   delete(prId) {
-    if (LOG_QUERIES) console.log(`db: DELETE ${prId}`)
     return new Promise((resolve, reject) => {
-      this.db.run(`DELETE FROM pulls WHERE (prId = ?)`, prId, promiseCb(resolve, reject))
+      if (LOG_QUERIES) console.log(`db: DELETE FROM pulls WHERE prId = ?`, prId)
+      this.db.run(`DELETE FROM pulls WHERE prId = ?`, prId, promiseCb(resolve, reject))
     })
   }
 
