d78b3ad92cde503398f770fbaccc0f09097a9469
Jason Benn <jasoncbenn@gmail.com>
2017-05-11 01:05:55 -0700

    Super helpful command/script for testing AWS commands.

---
+++ scripts/test-aws.js
@@ -0,0 +1,27 @@
+import { readJSON } from '../src/utils'
+import path from 'path'
+import AWS from '../src/aws'
+
+
+readJSON('./config/picasso.json').then(config => {
+  const aws = new AWS(config)
+
+  // const domainName = "qa-jb-bogus-test-pr.minervaproject.com"
+  // const publicIp = "54.245.189.101"
+  // const instanceId = "51231c20-ffb5-4f82-965e-67ddfafb76e7"
+  // const dbName = "jb_bogus_test_pr"
+
+  // aws.describeInstance("b6352f7d-8891-45b1-94cf-8147fd782f43").then(data => console.log(data) })
+  // aws.createInstance("test").then(data => console.log(data))
+  // aws.startInstance("b6352f7d-8891-45b1-94cf-8147fd782f43").then(data => console.log(data))
+  // aws.deleteInstance("20ace0a2-bb0b-4f1e-bac2-82a9e6c0438f").then(data => console.log(data))
+  // aws.stopInstance("b6352f7d-8891-45b1-94cf-8147fd782f43").then(data => console.log(data))
+  // aws.createRoute53Record(domainName, publicIp).then(data => console.log(data))
+  // aws.deleteRoute53Record(domainName, publicIp).then(data => console.log(data))
+  // aws.deployInstance({ instanceId, domainName, dbName }).then(({DeploymentId}) => {
+  //   aws.describeDeployment(DeploymentId).then(data => {
+  //     console.log(data)
+  //     console.log(data.Deployments[0].Status)
+  //   })
+  // })
+})
