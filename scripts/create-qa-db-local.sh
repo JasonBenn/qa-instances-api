2b59b6472a7d0a7e47c4a0a194f1471a6c2e0266
Jason Benn <jasoncbenn@gmail.com>
2017-05-16 22:02:58 -0700

    Speed up DB script running locally.

--- scripts/create-qa-db-local.sh
+++ scripts/create-qa-db-local.sh
@@ -1,10 +1,10 @@
 echo "working 1" 1>&2
-sleep 0.5
+sleep 0.2
 echo "working 2" 1>&2
-sleep 0.5
+sleep 0.2
 echo "working 3" 1>&2
-sleep 0.5
+sleep 0.2
 echo "working 4" 1>&2
-sleep 0.5
+sleep 0.2
 echo "working 5" 1>&2
-sleep 0.5
+sleep 0.2
